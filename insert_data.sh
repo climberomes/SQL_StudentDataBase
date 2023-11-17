91cc984259785b3fee79e333d273a7c07274734c
climberomes <126999678+climberomes@users.noreply.github.com>
2023-11-17 12:47:03 -0600

    Update insert_data.sh
    gets the data from courses.csv

--- insert_data.sh
+++ insert_data.sh
@@ -1,59 +1,55 @@
-
 #!/bin/bash
-#declare -p IFS=","
 
-#Script to insert data from courses.csv and students.csv into students database
+# Script to insert data from courses.csv and students.csv into students database
 
-#PSQL variable to log in and interact with database
-#This will allow yus to query the database from the script.
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+echo $($PSQL "TRUNCATE students, majors, courses, majors_courses")
 
-cat courses.csv | while IFS="," read MAJOR COURSE
-#cat courses.csv | while IFS="," read MAJOR COURSE
+cat courses_test.csv | while IFS="," read MAJOR COURSE
 do
-if [[ $MAJOR != major ]]
-then
-  #get major_id
-  MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
-
-  #if not found
-  # checks if MAJOR_ID is not empty
-  if [[ -z $MAJOR_ID ]]
+  if [[ $MAJOR != "major" ]]
   then
-    #insert major
-    INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
-
-    #Output successful add
-    if [[ $INSERT_MAJOR_RESULT == "INSERT 0 1" ]]
-    then
-      echo Inserted into majors, $MAJOR
-    fi
-
-    #get new major_id
+    # get major_id
     MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
-  fi
-  
-  #get course_id
-  COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
 
-  #if not found
-  # checks if MAJOR_ID is not empty
-  if [[ -z $COURSE_ID ]]
-  then
-    #insert course
-    INSERT_COURSE_RESULT=$($PSQL "INSERT INTO courses(course) VALUES('$COURSES')")
-
-    #Output successful add
-    if [[ $INSERT_COURSE_RESULT == "INSERT 0 1" ]]
+    # if not found
+    if [[ -z $MAJOR_ID ]]
     then
-      echo Inserted into courses, $COURSES
+      # insert major
+      INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
+      if [[ $INSERT_MAJOR_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted into majors, $MAJOR
+      fi
+
+      # get new major_id
+      MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
     fi
 
-    #get new course_id
+    # get course_id
     COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
-  fi
-  
-  #insert into majors_courses
 
-fi
+    # if not found
+    if [[ -z $COURSE_ID ]]
+    then
+      # insert course
+      INSERT_COURSE_RESULT=$($PSQL "INSERT INTO courses(course) VALUES('$COURSE')")
+
+      if [[ $INSERT_COURSE_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted into courses, $COURSE
+      fi
+
+      # get new course_id
+      COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
+    fi
+
+    # insert into majors_courses
+    INSERT_MAJORS_COURSES_RESULT=$($PSQL "INSERT INTO majors_courses(major_id, course_id) VALUES($MAJOR_ID, $COURSE_ID)")
+  
+    if [[ $INSERT_MAJORS_COURSES_RESULT == "INSERT 0 1" ]]
+     then
+       echo Inserted into majors_courses, $MAJOR : $COURSE
+    fi
+  fi
 done
