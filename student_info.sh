3947014258f7bbad89215428b6115628635f53a4
climberomes <126999678+climberomes@users.noreply.github.com>
2023-11-29 17:00:09 -0600

    Update student_info.sh
    Joining multiple tables and filtering out courses with no students enrolled, or specific student enrolled

--- student_info.sh
+++ student_info.sh
@@ -32,3 +32,6 @@ echo "$($PSQL "SELECT major_id, COUNT(*) AS number_of_students, ROUND(AVG(gpa), 
 
 echo -e "\nList of majors, in alphabetical order, that either no student is taking or has a student whose first name contains a case insensitive 'ma':"
 echo "$($PSQL "SELECT major FROM majors FULL JOIN students ON majors.major_id = students.major_id WHERE major IS NOT NULL AND (student_id IS NULL OR first_name ILIKE '%ma%') ORDER BY major")"
+
+echo -e "\nList of unique courses, in reverse alphabetical order, that no student or 'Obie Hilpert' is taking:"
+echo "$($PSQL "SELECT DISTINCT(course) FROM students FULL JOIN majors USING(major_id) FULL JOIN majors_courses USING (major_id) FULL JOIN courses USING(course_id) WHERE (student_id IS NULL OR (first_name LIKE 'Obie' AND  last_name LIKE 'Hilpert')) ORDER BY course DESC")"
