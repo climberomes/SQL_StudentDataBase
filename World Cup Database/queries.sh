dc41b74d6b4bd7adfffa6e30a1f3ee81d4a3b048
climberomes <126999678+climberomes@users.noreply.github.com>
2023-12-01 11:22:57 -0600

    Update queries.sh
    hot fix. Test case needed to be in ascending order.

--- World Cup Database/queries.sh
+++ World Cup Database/queries.sh
@@ -26,13 +26,7 @@ echo -e "\nNumber of games where the winning team scored more than two goals:"
 echo "$($PSQL "SELECT count(winner_goals) FROM games WHERE winner_goals>2")"
 
 echo -e "\nWinner of the 2018 tournament team name:"
-echo "$($PSQL "
-SELECT name 
-FROM games 
-LEFT JOIN teams 
-ON games.winner_id = teams.team_id 
-WHERE round LIKE 'Final' AND year = 2018;
-")"
+echo "$($PSQL "SELECT name FROM games LEFT JOIN teams ON games.winner_id = teams.team_id WHERE round LIKE 'Final' AND year = 2018")"
 
 #
 # This was the hard one
@@ -46,32 +40,13 @@ WHERE round LIKE 'Final' AND year = 2018;
 # Duplicate the data but display the name of Winner_id (half the table) 
 # then Opponent_id (the other half)
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo "$($PSQL "
-SELECT DISTINCT(t.name)
-FROM teams t 
-LEFT JOIN (games g1 LEFT JOIN games g2 ON g1.game_id=g2.game_id)
-ON t.team_id=g1.winner_id OR t.team_id=g2.opponent_id
-WHERE g1.year=2014 AND g1.round='Eighth-Final'
-ORDER BY t.name;
-")"
+echo "$($PSQL "SELECT DISTINCT(t.name) FROM teams t LEFT JOIN (games g1 LEFT JOIN games g2 ON g1.game_id=g2.game_id) ON t.team_id=g1.winner_id OR t.team_id=g2.opponent_id WHERE g1.year=2014 AND g1.round='Eighth-Final' ORDER BY t.name")"
 
 echo -e "\nList of unique winning team names in the whole data set:"
-echo "$($PSQL "
-SELECT DISTINCT(name) 
-FROM games 
-LEFT JOIN teams 
-ON games.winner_id = teams.team_id;
-")"
+echo "$($PSQL "SELECT DISTINCT(name) FROM games LEFT JOIN teams ON games.winner_id = teams.team_id ORDER BY name")"
 
 echo -e "\nYear and team name of all the champions:"
-echo "$($PSQL "
-SELECT year,name 
-FROM games 
-LEFT JOIN teams 
-ON games.winner_id = teams.team_id 
-WHERE round LIKE 'Final' 
-ORDER BY year;
-")"
+echo "$($PSQL "SELECT year,name FROM games LEFT JOIN teams ON games.winner_id = teams.team_id WHERE round LIKE 'Final' ORDER BY year")"
 
 echo -e "\nList of teams that start with 'Co':"
 echo "$($PSQL "SELECT name FROM teams WHERE name LIKE 'Co%'")"
